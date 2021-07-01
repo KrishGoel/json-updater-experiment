ce670203a9c58cb81f58a051abf6680f34a5ff35
Krish Goel <krishgoel3@gmail.com>
2021-07-01 19:29:55 +0530

    Initialisation commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const writeJsonFile = require('write-json-file');
+
+(async () => {
+    try {
+        await writeJsonFile('./test.json', {result: "successful"})
+        console.log("Successsfully updated JSON")
+    }
+    catch(err) {
+        console.log("Error: " + err)
+    }
+})();
